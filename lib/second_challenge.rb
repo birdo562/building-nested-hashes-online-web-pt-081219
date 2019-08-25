f513d4111432eed9848e58561bea72eeeeccbbdb
Roberto Medina <robby.medina562@gmail.com>
2019-08-25 05:11:16 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,7 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarch => {}
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
+    :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => {}}, 
+    :capulet => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends =>
       }}
 
   
